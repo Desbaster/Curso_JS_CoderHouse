a1721d0cf81935df1f69e4bcffe2ba775441d682
joel paredes <desbaster@gmail.com>
2021-03-04 20:21:14 -0300

    fucionando todos los desafios

--- js/main.js
+++ js/main.js
@@ -1,32 +1,34 @@
-// Declaracion y asignacion de variables
-// alert ("¡Hola!, te pedimos que por favor ingrese los siguientes datos:");
-// let nombre = prompt ("Ingrese su Nombre:");
-// let apellido = prompt ("Ingrese su Apellido:");
-// let edadTexto = prompt ("Ingrese su Edad:");
-// let cumpleanios = prompt ("¿Ya cumpliste año?")
-// const año = 2021;
+//Declaracion y asignacion de variables
+alert ("¡Hola!, te pedimos que por favor ingrese los siguientes datos:");
+let nombre = prompt ("Ingrese su Nombre:");
+let apellido = prompt ("Ingrese su Apellido:");
+let edadTexto = prompt ("Ingrese su Edad:");
+let cumpleanios = prompt ("¿Ya cumpliste año?")
+const año = 2021;
 
 //Conversion
-//let edadPrompt = parseInt(edadTexto);
+let edadPrompt = parseInt(edadTexto);
 
 //Concatenar nombre y apellido
-//let nombreCompleto = (nombre + " " + apellido);
+let nombreCompleto = (nombre + " " + apellido);
 
-//if (cumpleanios == "si" || cumpleanios == "Si" || cumpleanios == "SI" || cumpleanios == "sI") {
-//    var añoNacimiento = (año - edadPrompt);
-//    alert("Hola " + nombreCompleto + ", tu año de nacimiento es: " + añoNacimiento +  ".");
-//}else {
-//    var añoNacimiento1 = (año - edadPrompt - 1);
-//    alert("Hola " + nombreCompleto + ", tu año de nacimiento es: " + añoNacimiento1 + ".");
-//}
-//
-alert ("Bienvenido, el día de hoy estamos relizando un sorteo en el que obtendras un fabuloso premio, debes elegir un numero entre 1 y 100, tendras tres oportunidades para acertar, SUERTE!")
+if (cumpleanios == "si" || cumpleanios == "Si" || cumpleanios == "SI" || cumpleanios == "sI") {
+    var añoNacimiento = (año - edadPrompt);
+    alert("Hola " + nombreCompleto + ", tu año de nacimiento es: " + añoNacimiento +  ".");
+}else {
+    var añoNacimiento1 = (año - edadPrompt - 1);
+    alert("Hola " + nombreCompleto + ", tu año de nacimiento es: " + añoNacimiento1 + ".");
+}
+
+alert ("Bienvenido "+ nombreCompleto + ", el día de hoy estamos relizando un sorteo en el que obtendras un fabuloso premio, debes elegir un numero entre 1 y 100, tendras tres oportunidades para acertar, SUERTE!");
 
 for (sorteo=1; sorteo<=3; sorteo++) {
     let numero = parseInt(prompt ("Ingresa tu numero, SUERTE!:"));
-    if (numero == 21) {
+    if (numero == edadPrompt) {
         alert("Felicitaciones eres el afortunado ganador de nuestro premio");
         break;
     }
-    alert(numero + " no es el numero premiado.")
+    alert(numero + " no es el numero premiado.");
 }
+
+
