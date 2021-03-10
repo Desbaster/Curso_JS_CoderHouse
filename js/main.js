13e1b4afd8220608dcd3951e132194da62e73084
joel paredes <desbaster@gmail.com>
2021-03-09 21:40:38 -0300

    cambios de gramatia

--- js/main.js
+++ js/main.js
@@ -15,11 +15,11 @@ const redondo = resultado.toFixed(2);
 console.log(redondo);
 
 if (resultado < 18.5) {
-    alert (nombre + " tu IMC= " + redondo + " y es demasiado bajo, consulte a su nutricionista");
+    alert (nombre + " tu IMC= " + redondo + " y es demasiado bajo, consulta a tu nutricionista");
     } else if (resultado >= 18.5 && resultado < 25){
         alert (nombre + " tu IMC= " + redondo + " y es normal, manten una dieta saludable.");
     } else if (resultado >= 25 && resultado < 30){
-        alert (nombre + " tu IMC= " + redondo + " y tienes Sobrepeso , consulte a su nutricionista.");    
+        alert (nombre + " tu IMC= " + redondo + " y tienes sobrepeso , consulte a tu nutricionista.");    
     }  else if (resultado >= 30){
-        alert (nombre + " tu IMC= " + redondo + " y  estas obeso, debes consultar al nutricionista.");
+        alert (nombre + " tu IMC= " + redondo + " y  estas obeso, debes consultar a tu nutricionista.");
     }  
