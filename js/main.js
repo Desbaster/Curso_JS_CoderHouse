312dca925ce18a7fa906534c10ff052b14be8115
joel paredes <desbaster@gmail.com>
2021-03-09 21:36:28 -0300

    no me gusta este ejercicio

--- js/main.js
+++ js/main.js
@@ -1,34 +1,25 @@
-//Declaracion y asignacion de variables
-alert ("¡Hola!, te pedimos que por favor ingrese los siguientes datos:");
-let nombre = prompt ("Ingrese su Nombre:");
-let apellido = prompt ("Ingrese su Apellido:");
-let edadTexto = prompt ("Ingrese su Edad:");
-let cumpleanios = prompt ("¿Ya cumpliste año?")
-const año = 2021;
-
-//Conversion
-let edadPrompt = parseInt(edadTexto);
-
-//Concatenar nombre y apellido
-let nombreCompleto = (nombre + " " + apellido);
-
-if (cumpleanios == "si" || cumpleanios == "Si" || cumpleanios == "SI" || cumpleanios == "sI") {
-    var añoNacimiento = (año - edadPrompt);
-    alert("Hola " + nombreCompleto + ", tu año de nacimiento es: " + añoNacimiento +  ".");
-}else {
-    var añoNacimiento1 = (año - edadPrompt - 1);
-    alert("Hola " + nombreCompleto + ", tu año de nacimiento es: " + añoNacimiento1 + ".");
+// Funcion
+function peso (kg,m){
+    return kg / (m * m);    
 }
 
-alert ("Bienvenido "+ nombreCompleto + ", el día de hoy estamos relizando un sorteo en el que obtendras un fabuloso premio, debes elegir un numero entre 1 y 100, tendras tres oportunidades para acertar, SUERTE!");
+//Valores de entrada
+const nombre = prompt("Ingresa tu Nombre?");
+const pesoEnKg = parseInt (prompt("Ingresa tu peso en kilogramos?"));
+const altura = parseFloat (prompt("Ingresa tu estatura en metros?"));
 
-for (sorteo=1; sorteo<=3; sorteo++) {
-    let numero = parseInt(prompt (sorteo + ". Ingresa tu numero, SUERTE!:"));
-    if (numero == edadPrompt) {
-        alert("Felicitaciones eres el afortunado ganador de nuestro premio");
-        break;
-    }
-    alert(numero + " no es el numero premiado.");
-}
+//Calculo y convercion
+const resultado = peso(pesoEnKg, altura);
+const redondo = resultado.toFixed(2);
 
+console.log(redondo);
 
+if (resultado < 18.5) {
+    alert (nombre + " tu IMC= " + redondo + " y es demasiado bajo, consulte a su nutricionista");
+    } else if (resultado >= 18.5 && resultado < 25){
+        alert (nombre + " tu IMC= " + redondo + " y es normal, manten una dieta saludable.");
+    } else if (resultado >= 25 && resultado < 30){
+        alert (nombre + " tu IMC= " + redondo + " y tienes Sobrepeso , consulte a su nutricionista.");    
+    }  else if (resultado >= 30){
+        alert (nombre + " tu IMC= " + redondo + " y  estas obeso, debes consultar al nutricionista.");
+    }  
