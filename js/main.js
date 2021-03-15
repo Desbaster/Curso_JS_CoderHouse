a6f5e167ecb3ee48d1053faaf2fcc3cb4e6d9232
joel paredes <desbaster@gmail.com>
2021-03-14 22:59:27 -0300

    arrys listo

--- js/main.js
+++ js/main.js
@@ -1,42 +1,64 @@
+// Declaramos Arrays
+const nombres = [];
+const pesos = [];
+const alturas = [];
+let cantidad = 2; 
+
 class Paciente {
     constructor(nombre, pesoEnKg, altura) {
         this.nombre = nombre;
         this.pesoEnKg = pesoEnKg;
         this.altura = altura;
     }
-    ingresarNombre () {
-        this.nombre = prompt("Ingresa tu Nombre?");
-    }
-    ingresarPeso () {
-        this.pesoEnKg = parseInt (prompt("Ingresa tu peso en kilogramos?"));
-    }
-    ingresarAltura () {
-        this.altura = parseFloat (prompt("Ingresa tu estatura en metros?"));
-    }
 }
 
-const Paciente1 = new Paciente("joel", 65, 1.70);
-Paciente1.ingresarNombre();
-Paciente1.ingresarPeso();
-Paciente1.ingresarAltura();
+do {
+    let nombre = prompt("Ingresa tu Nombre?");
+    if (nombre == ""){
+            break;
+        }
+    let pesoEnKg = parseInt (prompt("Ingresa tu peso en kilogramos?"));
+    let altura = parseFloat (prompt("Ingresa tu estatura en metros?"));
+    nombres.push (nombre);
+    pesos.push (pesoEnKg);
+    alturas.push (altura);
 
-let upperNombre = Paciente1["nombre"].toUpperCase();
+}while (nombres.length != cantidad)
+
+console.log (nombres[0]);
+console.log (nombres[1]);
+console.log (pesos[0]);
+console.log (pesos[1]);
+console.log (alturas[0]);
+console.log (alturas[1]);
 
 //Funcion
-function peso (kg,m){
+function imc (kg,m){
     return kg / (m * m);    
 }
 
 //Calculo y convercion
-const resultado = peso(Paciente1.pesoEnKg , Paciente1.altura);
-const redondo = resultado.toFixed(2);
+const resultado1 = imc(pesos[0] , alturas[0]);
+const resultado2 = imc(pesos[1] , alturas[1]);
+const redondo1 = resultado1.toFixed(2);
+const redondo2 = resultado2.toFixed(2);
 
-if (resultado < 18.5) {
-    alert ("Hola " + upperNombre + " tu IMC= " + redondo + " y es demasiado bajo, consulta a tu nutricionista");
-    } else if (resultado >= 18.5 && resultado < 25) {
-        alert ("Hola " + upperNombre + " tu IMC= " + redondo + " y es normal, manten una dieta saludable.");
-    } else if (resultado >= 25 && resultado < 30) {
-        alert ("Hola " + upperNombre + " tu IMC= " + redondo + " y tienes sobrepeso , consulte a tu nutricionista.");    
-    }  else if (resultado >= 30) {
-        alert ("Hola " + upperNombre + " tu IMC= " + redondo + " y  estas obeso, debes consultar a tu nutricionista.");
+if (resultado1 < 18.5) {
+    alert ("Hola " + nombres[0].toUpperCase() + " tu IMC= " + redondo1 + " y es demasiado bajo, consulta a tu nutricionista");
+    } else if (resultado1 >= 18.5 && resultado1 < 25) {
+        alert ("Hola " + nombres[0].toUpperCase() + " tu IMC= " + redondo1 + " y es normal, manten una dieta saludable.");
+    } else if (resultado1 >= 25 && resultado1 < 30) {
+        alert ("Hola " + nombres[0].toUpperCase() + " tu IMC= " + redondo1 + " y tienes sobrepeso , consulte a tu nutricionista.");    
+    }  else if (resultado1 >= 30) {
+        alert ("Hola " + nombres[0].toUpperCase() + " tu IMC= " + redondo1 + " y  estas obeso, debes consultar a tu nutricionista.");
     } 
+
+    if (resultado2 < 18.5) {
+        alert ("Hola " + nombres[1].toUpperCase() + " tu IMC= " + redondo2 + " y es demasiado bajo, consulta a tu nutricionista");
+        } else if (resultado2 >= 18.5 && resultado2 < 25) {
+            alert ("Hola " + nombres[1].toUpperCase() + " tu IMC= " + redondo2 + " y es normal, manten una dieta saludable.");
+        } else if (resultado2 >= 25 && resultado2 < 30) {
+            alert ("Hola " + nombres[1].toUpperCase() + " tu IMC= " + redondo2 + " y tienes sobrepeso , consulte a tu nutricionista.");    
+        }  else if (resultado2 >= 30) {
+            alert ("Hola " + nombres[1].toUpperCase() + " tu IMC= " + redondo2 + " y  estas obeso, debes consultar a tu nutricionista.");
+        } 
